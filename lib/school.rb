976a558977a53a02bfa41c6e0c03aae02162d22c
David Ingram <david@ingramaviation.com>
2019-08-20 00:21:59 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,20 @@
 # code here!
+class School
+  attr_accessor :name, :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end 
+  
+  def roster
+    @roster
+  end 
+  
+  def add_student(name, grade)
+    
+    @roster << 
+  end
+  
+  
+end   
